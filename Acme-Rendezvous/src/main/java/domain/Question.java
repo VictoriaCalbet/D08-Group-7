62936559085546e1068a56dc33924875edc6fd71
Guillermo Alcalá Gamero <guillermo.ag.95@gmail.com>
2018-02-25 21:40:10 +0100

    Missing cascade on question's answers

--- Acme-Rendezvous/src/main/java/domain/Question.java
+++ Acme-Rendezvous/src/main/java/domain/Question.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 
 import javax.persistence.Access;
 import javax.persistence.AccessType;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
@@ -43,7 +44,7 @@ public class Question extends DomainEntity {
 
 	@Valid
 	@NotNull
-	@OneToMany(mappedBy = "question")
+	@OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
 	public Collection<Answer> getAnswers() {
 		return this.answers;
 	}
