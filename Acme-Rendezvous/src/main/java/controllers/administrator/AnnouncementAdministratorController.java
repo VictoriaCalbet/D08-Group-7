35f7468b7d21df3b3d4d83cf474e363c8aaec455
Juan Carlos Utrilla <utri1990gmail.com>
2018-02-25 09:40:52 +0100

    Converters - Corregido la acción de borrar (delete.do)

--- Acme-Rendezvous/src/main/java/controllers/administrator/AnnouncementAdministratorController.java
+++ Acme-Rendezvous/src/main/java/controllers/administrator/AnnouncementAdministratorController.java
@@ -6,9 +6,9 @@ import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import services.AnnouncementService;
@@ -55,16 +55,15 @@ public class AnnouncementAdministratorController extends AbstractController {
 
 	// Edition    -----------------------------------------------------------
 
-	@RequestMapping(value = "/delete", method = RequestMethod.POST, params = "delete")
-	public ModelAndView delete(final Announcement announcement, final BindingResult binding) {
+	@RequestMapping(value = "/delete", method = RequestMethod.GET)
+	public ModelAndView delete(@RequestParam final int announcementId) {
 		ModelAndView result = null;
+		Announcement announcement = null;
 
-		try {
-			this.announcementService.delete(announcement);
-			result = new ModelAndView("redirect:/announcement/administrator/list.do");
-		} catch (final Throwable oops) {
-			result = this.createEditModelAndView(announcement, "announcement.commit.error");
-		}
+		announcement = this.announcementService.findOne(announcementId);
+
+		this.announcementService.delete(announcement);
+		result = new ModelAndView("redirect:/announcement/administrator/list.do");
 
 		return result;
 	}
