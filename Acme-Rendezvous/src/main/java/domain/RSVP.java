75f413afb89020df525b92e2c79b6376696ba55c
Guillermo Alcalá Gamero <guillermo.ag.95@gmail.com>
2018-02-27 10:00:47 +0100

    Query efficiency

--- Acme-Rendezvous/src/main/java/domain/RSVP.java
+++ Acme-Rendezvous/src/main/java/domain/RSVP.java
@@ -4,12 +4,17 @@ package domain;
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 @Entity
 @Access(AccessType.PROPERTY)
+@Table(indexes = {
+	@Index(columnList = "isCancelled")
+})
 public class RSVP extends DomainEntity {
 
 	// Attributes -------------------------------------------------------------
--- Acme-Rendezvous/src/main/java/domain/Rendezvous.java
+++ Acme-Rendezvous/src/main/java/domain/Rendezvous.java
@@ -8,9 +8,11 @@ import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
@@ -24,6 +26,9 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Access(AccessType.PROPERTY)
+@Table(indexes = {
+	@Index(columnList = "isDeleted"), @Index(columnList = "isDraft"), @Index(columnList = "isAdultOnly"), @Index(columnList = "meetingMoment")
+})
 public class Rendezvous extends DomainEntity {
 
 	// Attributes -------------------------------------------------------------
